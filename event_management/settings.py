291782eeaedd496c4eba40774e5abe394d64166b
sheikhismam <sheikhismam.bgctub@gmail.com>
2025-10-27 10:25:32 +0600

    Database setup doen

--- event_management/settings.py
+++ event_management/settings.py
@@ -83,25 +83,25 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default=''),
-        'USER': config('DB_USER', default=''),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', cast=int)
-    }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_management_7_user:N7RecZoQGXUiAMMJwkC41P7Z0bbi9oho@dpg-d3vd22ndiees73esvktg-a.oregon-postgres.render.com/event_management_7',
-#         conn_max_age=600
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME', default=''),
+#         'USER': config('DB_USER', default=''),
+#         'PASSWORD': config('DB_PASSWORD', default=''),
+#         'HOST': config('DB_HOST', default='localhost'),
+#         'PORT': config('DB_PORT', cast=int)
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_manager_kc85_user:ldOA0SACtOTJbVjcoOHwsArjlJo0udGR@dpg-d3vf74vdiees73euch8g-a.oregon-postgres.render.com/event_manager_kc85',
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
